281736e99566140de308e247e2ae900ad5a2d3b3
Rafhael Gomes <rafhael@protonmail.com>
2022-07-24 20:22:49 -0300

    feat: ensure SignupController is defined

--- src/controllers/test.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Teste', () => {
-  it('testando', () => {
-    expect(1).toBe(1)
-  })
-})
---
+++ src/presentation/controllers/signup/index.ts
@@ -0,0 +1,5 @@
+export class SignupController {
+  async handle(): Promise<void> {
+    return await Promise.resolve()
+  }
+}
