77bf7eb00b7013d20928c0159467624a13aa39df
Rafhael Gomes <rafhael@protonmail.com>
2022-07-24 20:46:27 -0300

    test: ensure SignupController returns status 400 when email is not provided

--- src/presentation/controllers/signup/index.spec.ts
+++ src/presentation/controllers/signup/index.spec.ts
@@ -1,4 +1,14 @@
 import { SignupController } from '@/presentation/controllers/signup'
+import { faker } from '@faker-js/faker'
+
+const fakePassword = faker.internet.password()
+const httpRequest = {
+  body: {
+    name: faker.name.findName(),
+    password: fakePassword,
+    passwordConfirmation: fakePassword
+  }
+}
 
 describe('SignupController', () => {
   it('should be defined', () => {
@@ -11,5 +21,15 @@ describe('SignupController', () => {
     const sut = new SignupController()
 
     expect(sut.handle).toBeDefined()
+  })
+
+  it('should return status 400 when email is not provided', async () => {
+    const sut = new SignupController()
+
+    const promise = await sut.handle(httpRequest as any)
+
+    const httpResponse = { body: { statusCode: 400 } }
+
+    expect(promise).toEqual(httpResponse)
   })
 })
