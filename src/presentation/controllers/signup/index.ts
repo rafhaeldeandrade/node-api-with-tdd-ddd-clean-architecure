8424769983fddab3366038497c81b457103f933c
Rafhael Gomes <rafhael@protonmail.com>
2022-07-26 17:06:34 -0300

    refactor: returns badRequest helper function

--- src/presentation/controllers/signup/index.ts
+++ src/presentation/controllers/signup/index.ts
@@ -1,20 +1,15 @@
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignupController {
   async handle(params: httpRequest): Promise<httpResponse> {
     if (!params.body?.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!params.body?.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
 
     return await Promise.resolve({ statusCode: 200 })
