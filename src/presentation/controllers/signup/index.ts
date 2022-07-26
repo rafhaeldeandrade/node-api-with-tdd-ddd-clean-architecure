fcb058bcac12964b897a158e1fd181a69f06809b
Rafhael Gomes <rafhael@protonmail.com>
2022-07-26 19:52:52 -0300

    refactor: change missing params verification to an for of iterating an array of requiredParams

--- src/presentation/controllers/signup/index.ts
+++ src/presentation/controllers/signup/index.ts
@@ -3,15 +3,15 @@ import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignupController {
-  async handle(params: httpRequest): Promise<httpResponse> {
-    if (!params.body?.name) {
-      return badRequest(new MissingParamError('name'))
+  async handle(params: httpRequest): Promise<httpResponse | boolean> {
+    const requiredParams = ['name', 'email']
+
+    for (const param of requiredParams) {
+      if (!params.body[param as keyof typeof params.body]) {
+        return badRequest(new MissingParamError(param))
+      }
     }
 
-    if (!params.body?.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    return await Promise.resolve({ statusCode: 200 })
+    return true
   }
 }
