69000823736c8a347999f01890bad13b8af4d418
Rafhael Gomes <rafhael@protonmail.com>
2022-07-26 20:03:35 -0300

    feat: ensure SignupController returns 400 if passwordConfirmation is not provided

--- src/presentation/controllers/signup/index.ts
+++ src/presentation/controllers/signup/index.ts
@@ -4,7 +4,7 @@ import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignupController {
   async handle(params: httpRequest): Promise<httpResponse | boolean> {
-    const requiredParams = ['name', 'email', 'password']
+    const requiredParams = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const param of requiredParams) {
       if (!params.body[param as keyof typeof params.body]) {
