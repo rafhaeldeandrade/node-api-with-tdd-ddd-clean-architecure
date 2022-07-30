6c4c69b25d5be69e750a62bf816314777c05b18e
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 15:37:07 -0300

    feat: add serverError helper function

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
 import { httpResponse } from '@/presentation/contracts/http'
+import { ServerError } from '@/presentation/errors/server-error'
 
 export function badRequest(error: Error): httpResponse {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export function serverError(): httpResponse {
+  return {
+    statusCode: 500,
+    body: new ServerError()
   }
 }
