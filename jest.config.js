76f585d2229c261e52a784d25c406cf87d251232
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 19:04:42 -0300

    chore: add some test scripts and config files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
