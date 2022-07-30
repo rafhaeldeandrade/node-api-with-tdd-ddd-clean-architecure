0ca2608330627631ece5617b48f1b4e51ea9390f
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 15:37:37 -0300

    feat: ensure SignupController returns 500 if something crashes

--- src/presentation/controllers/signup/index.ts
+++ src/presentation/controllers/signup/index.ts
@@ -3,36 +3,45 @@ import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { MailValidator } from '@/presentation/contracts/mail-validator'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-import { badRequest } from '@/presentation/helpers/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 
 export class SignupController implements Controller<httpRequest, httpResponse> {
   constructor(private readonly mailValidator: MailValidator) {}
 
   async handle(params: httpRequest): Promise<httpResponse> {
-    const requiredParams = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredParams = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
 
-    for (const param of requiredParams) {
-      if (!params.body[param as keyof typeof params.body]) {
-        return badRequest(new MissingParamError(param))
+      for (const param of requiredParams) {
+        if (!params.body[param as keyof typeof params.body]) {
+          return badRequest(new MissingParamError(param))
+        }
       }
-    }
 
-    const { email, password, passwordConfirmation } = params.body
-    if (password !== passwordConfirmation) {
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-    }
-
-    const mailIsValid = this.mailValidator.isValid(email)
-
-    if (!mailIsValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        message: 'ok'
+      const { email, password, passwordConfirmation } = params.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
+
+      const mailIsValid = this.mailValidator.isValid(email)
+
+      if (!mailIsValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      return {
+        statusCode: 200,
+        body: {
+          message: 'ok'
+        }
+      }
+    } catch (_error) {
+      return serverError()
     }
   }
 }
