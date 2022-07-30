33a939bb92893af2f45147540b009f9859d740be
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 19:13:55 -0300

    test: ensure EmailValidatorAdapter has an isValid method

--- src/utils/email-validator/index.spec.ts
+++ src/utils/email-validator/index.spec.ts
@@ -4,4 +4,8 @@ describe('EmailValidator', () => {
   it('should be defined', () => {
     expect(new EmailValidatorAdapter()).toBeDefined()
   })
+
+  it('should have an isValid method', () => {
+    expect(new EmailValidatorAdapter().isValid).toBeDefined()
+  })
 })
