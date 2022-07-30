81f512f0ba5fcad3e024352beb844eca65649ddf
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 19:22:53 -0300

    feat: ensure EmailValidatorAdapter returns false when validator returns false

--- src/utils/email-validator/index.ts
+++ src/utils/email-validator/index.ts
@@ -1,6 +1,7 @@
+import { EmailValidator } from '@/presentation/contracts/email-validator'
 import validator from 'validator'
 
-export class EmailValidatorAdapter implements EmailValidatorAdapter {
+export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
     const emailValidator = validator.isEmail(email)
 
