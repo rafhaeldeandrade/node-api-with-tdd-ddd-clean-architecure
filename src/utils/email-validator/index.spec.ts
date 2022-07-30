6e9e006ce9fc066504beb302cd73ac8eecb17a2d
Rafhael Gomes <rafhael@protonmail.com>
2022-07-30 19:23:04 -0300

    test: ensure EmailValidatorAdapter returns false when validator returns false

--- src/utils/email-validator/index.spec.ts
+++ src/utils/email-validator/index.spec.ts
@@ -1,11 +1,33 @@
+import { EmailValidator } from '@/presentation/contracts/email-validator'
 import { EmailValidatorAdapter } from '@/utils/email-validator'
+import { faker } from '@faker-js/faker'
+import validator from 'validator'
+
+function makeSut(): { sut: EmailValidator } {
+  const sut = new EmailValidatorAdapter()
+
+  return { sut }
+}
 
 describe('EmailValidator', () => {
   it('should be defined', () => {
-    expect(new EmailValidatorAdapter()).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut).toBeDefined()
   })
 
   it('should have an isValid method', () => {
-    expect(new EmailValidatorAdapter().isValid).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut.isValid).toBeDefined()
+  })
+
+  it('should return false if validator returns false', () => {
+    const { sut } = makeSut()
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid(faker.internet.email())
+
+    expect(isValid).toBe(false)
   })
 })
