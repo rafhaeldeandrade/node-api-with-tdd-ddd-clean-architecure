62a1fa440c49125eccae001f2a1e83b25762ab13
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 17:36:56 -0300

    feat: ensure Argon2Adapter calls argon2 with the correct params

--- src/infra/cryptography/argon2-adapter.ts
+++ src/infra/cryptography/argon2-adapter.ts
@@ -1,7 +1,13 @@
 import { Encrypter } from '@/data/contracts/encrypter'
+import argon2 from 'argon2'
 
 export class Argon2Adapter implements Encrypter {
+  constructor(
+    private readonly argon2Options: argon2.Options & { raw?: false }
+  ) {}
+
   async encrypt(value: string): Promise<string> {
+    await argon2.hash(value, this.argon2Options)
     return ''
   }
 }
