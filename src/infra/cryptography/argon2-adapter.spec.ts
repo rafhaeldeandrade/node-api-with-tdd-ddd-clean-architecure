b9257d793b8d8cbed2fa829dfaf13899d3a2e5be
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 17:37:12 -0300

    test: ensure Argon2Adapter calls argon2 with the correct params

--- src/infra/cryptography/argon2-adapter.spec.ts
+++ src/infra/cryptography/argon2-adapter.spec.ts
@@ -1,12 +1,14 @@
 import { Encrypter } from '@/data/contracts/encrypter'
 import { Argon2Adapter } from '@/infra/cryptography/argon2-adapter'
+import { faker } from '@faker-js/faker'
+import argon2 from 'argon2'
 
 interface SutTypes {
   sut: Encrypter
 }
 
-function makeSut(): SutTypes {
-  const sut = new Argon2Adapter()
+function makeSut(options?: argon2.Options & { raw?: false }): SutTypes {
+  const sut = new Argon2Adapter(options ?? { raw: false })
 
   return {
     sut
@@ -23,5 +25,22 @@ describe('Argon2Adapter', () => {
     const { sut } = makeSut()
 
     expect(sut.encrypt).toBeDefined()
+  })
+
+  it('should call argon2 with the correct params', async () => {
+    const argon2Options = {
+      type: argon2.argon2id,
+      memoryCost: 37888,
+      parallelism: 1,
+      timeCost: 2
+    }
+    const { sut } = makeSut(argon2Options)
+
+    const hashSpy = jest.spyOn(argon2, 'hash')
+
+    const fakePassword = faker.internet.password()
+    await sut.encrypt(fakePassword)
+
+    expect(hashSpy).toHaveBeenCalledWith(fakePassword, argon2Options)
   })
 })
