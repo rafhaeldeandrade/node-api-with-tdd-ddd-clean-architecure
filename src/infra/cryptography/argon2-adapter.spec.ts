05d08c5bfd88cd51046d6ddb5cf2036cb4605742
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 17:23:25 -0300

    test: ensure Argon2Adapter has a method called encrypt

--- src/infra/cryptography/argon2-adapter.spec.ts
+++ src/infra/cryptography/argon2-adapter.spec.ts
@@ -1,7 +1,27 @@
+import { Encrypter } from '@/data/contracts/encrypter'
 import { Argon2Adapter } from '@/infra/cryptography/argon2-adapter'
+
+interface SutTypes {
+  sut: Encrypter
+}
+
+function makeSut(): SutTypes {
+  const sut = new Argon2Adapter()
+
+  return {
+    sut
+  }
+}
 
 describe('Argon2Adapter', () => {
   it('should be defined', () => {
-    expect(new Argon2Adapter()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
+  })
+
+  it('should have a method called encrypt', () => {
+    const { sut } = makeSut()
+
+    expect(sut.encrypt).toBeDefined()
   })
 })
