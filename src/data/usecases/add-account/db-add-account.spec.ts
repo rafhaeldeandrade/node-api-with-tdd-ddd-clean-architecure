c4e1161338350d1bc3ea053ce9b221ce9f79fb11
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 10:41:58 -0300

    test: refactor sut to a factory helper

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,24 @@
 import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { AddAccount } from '@/domain/usecases/add-account'
+
+interface SutTypes {
+  sut: AddAccount
+}
+
+function makeSut(): SutTypes {
+  const sut = new DbAddAccount()
+  return { sut }
+}
 
 describe('DbAddAccountUseCase', () => {
   it('should be defined', () => {
-    const sut = new DbAddAccount()
+    const { sut } = makeSut()
 
     expect(sut).toBeDefined()
   })
 
   it('should have a add method', () => {
-    const sut = new DbAddAccount()
+    const { sut } = makeSut()
 
     expect(sut.add).toBeDefined()
   })
