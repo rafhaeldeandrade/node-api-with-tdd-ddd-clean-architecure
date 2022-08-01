5d9d4ca2acee381f64af431f52dad1423bc6f52e
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 21:57:47 -0300

    test: ensure MongooseAddAccount has a method called add

--- src/infra/database/mongoose/add-account/index.spec.ts
+++ src/infra/database/mongoose/add-account/index.spec.ts
@@ -1,7 +1,27 @@
+import { AddAccountRepository } from '@/data/contracts/add-account-repository'
 import { MongooseAddAccount } from '@/infra/database/mongoose/add-account'
+
+interface SutTypes {
+  sut: AddAccountRepository
+}
+
+function makeSut(): SutTypes {
+  const sut = new MongooseAddAccount()
+  return {
+    sut
+  }
+}
 
 describe('MongooseAddAccount', () => {
   it('should be defined', () => {
-    expect(new MongooseAddAccount()).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut).toBeDefined()
+  })
+
+  it('should have a method called add', () => {
+    const { sut } = makeSut()
+
+    expect(sut.add).toBeDefined()
   })
 })
