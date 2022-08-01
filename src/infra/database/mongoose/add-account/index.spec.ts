9971a5832e9dc99633ac10c60aee40b1ca8e18a4
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 22:20:04 -0300

    test: ensure MongooseAddAccount calls mongoModel's create method with the correct params

--- src/infra/database/mongoose/add-account/index.spec.ts
+++ src/infra/database/mongoose/add-account/index.spec.ts
@@ -1,5 +1,7 @@
 import { AddAccountRepository } from '@/data/contracts/add-account-repository'
 import { MongooseAddAccount } from '@/infra/database/mongoose/add-account'
+import { mongooseAccountModel } from '@/infra/database/mongoose/schemas/account'
+import { faker } from '@faker-js/faker'
 
 interface SutTypes {
   sut: AddAccountRepository
@@ -23,5 +25,25 @@ describe('MongooseAddAccount', () => {
     const { sut } = makeSut()
 
     expect(sut.add).toBeDefined()
+  })
+
+  it("should call mongoModel's create method with the correct params", async () => {
+    const { sut } = makeSut()
+
+    const fakeId = faker.datatype.uuid()
+    const fakeAccount = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+
+    const createSpy = (mongooseAccountModel.create = jest
+      .fn()
+      .mockResolvedValueOnce({ _id: fakeId, ...fakeAccount }))
+
+    await sut.add(fakeAccount)
+
+    expect(createSpy).toHaveBeenCalledTimes(1)
+    expect(createSpy).toHaveBeenCalledWith(fakeAccount)
   })
 })
