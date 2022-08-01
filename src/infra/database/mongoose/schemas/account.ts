8c36ad214133ee33d1572a24e194db1da53bdc93
Rafhael Gomes <rafhael@protonmail.com>
2022-07-31 21:53:15 -0300

    feat: add mongoose's accountModel

---
+++ src/infra/database/mongoose/schemas/account.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const accountSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export const accountModel = mongoose.model('Account', accountSchema)
