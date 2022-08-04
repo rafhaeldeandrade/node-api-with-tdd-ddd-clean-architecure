3c85cb6f2c720ffeaa248ab3507b28b53d111c16
Rafhael Gomes <rafhael@protonmail.com>
2022-08-04 20:07:52 -0300

    test: ensure LogErrorDecoratorController call Controller's handle method

--- src/main/decorators/log-error-decorator.spec.ts
+++ src/main/decorators/log-error-decorator.spec.ts
@@ -1,15 +1,66 @@
 import { LogErrorDecoratorController } from '@/main/decorators/log-error-decorator'
+import { Controller } from '@/presentation/contracts/controller'
+import { httpRequest, httpResponse } from '@/presentation/contracts/http'
+import { faker } from '@faker-js/faker'
+
+interface SutTypes {
+  sut: Controller
+  controllerStub: Controller
+}
+
+const fakePassword = faker.internet.password()
+function makeHttpRequest(): httpRequest {
+  return {
+    body: {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: fakePassword,
+      passwordConfirmation: fakePassword
+    }
+  }
+}
+
+class ControllerStub implements Controller {
+  async handle(httpRequest: httpRequest): Promise<httpResponse> {
+    return {
+      statusCode: 200,
+      body: {
+        name: faker.name.findName()
+      }
+    }
+  }
+}
+
+function makeSut(): SutTypes {
+  const controllerStub = new ControllerStub()
+  const sut = new LogErrorDecoratorController(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
 
 describe('LogErrorDecoratorController', () => {
   it('should be defined', () => {
-    const sut = new LogErrorDecoratorController()
+    const { sut } = makeSut()
 
     expect(sut).toBeDefined()
   })
 
   it('should have a method called handle', () => {
-    const sut = new LogErrorDecoratorController()
+    const { sut } = makeSut()
 
     expect(sut.handle).toBeDefined()
+  })
+
+  it("should call a Controllers' handle method with the correct params", async () => {
+    const { sut, controllerStub } = makeSut()
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = makeHttpRequest()
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toHaveBeenCalledTimes(1)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
 })
