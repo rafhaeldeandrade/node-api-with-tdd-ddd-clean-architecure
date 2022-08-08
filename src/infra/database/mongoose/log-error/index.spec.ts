cf6a622db033a2aca62e6b384610350f9d9096c4
Rafhael Gomes <rafhael@protonmail.com>
2022-08-07 21:25:50 -0300

    test: ensure MongooseLogError calls logModel.create with the correct param

--- src/infra/database/mongoose/log-error/index.spec.ts
+++ src/infra/database/mongoose/log-error/index.spec.ts
@@ -1,11 +1,38 @@
+import { LogErrorRepository } from '@/data/contracts/log-error-repository'
 import { MongooseLogError } from '@/infra/database/mongoose/log-error'
+import { mongooseLogModel } from '@/infra/database/mongoose/schemas/log'
+import { faker } from '@faker-js/faker'
+
+interface SutTypes {
+  sut: LogErrorRepository
+}
+
+function makeSut(): SutTypes {
+  const sut = new MongooseLogError()
+  return {
+    sut
+  }
+}
 
 describe('MongooseLogError', () => {
   it('should be defined', () => {
-    expect(new MongooseLogError()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
   })
 
   it('should have a method called logError', () => {
-    expect(new MongooseLogError().logError).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.logError).toBeDefined()
+  })
+
+  it('should call logModel.create with the correct param', async () => {
+    const { sut } = makeSut()
+    mongooseLogModel.create = jest.fn()
+
+    const fakeStack = faker.lorem.sentence(10)
+    await sut.logError(fakeStack)
+
+    expect(mongooseLogModel.create).toHaveBeenCalledTimes(1)
+    expect(mongooseLogModel.create).toHaveBeenCalledWith({ stack: fakeStack })
   })
 })
