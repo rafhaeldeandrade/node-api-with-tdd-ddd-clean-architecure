2ee15ed1b0816f88044ddc51f0706b2b7b5eed22
Rafhael Gomes <rafhael@protonmail.com>
2022-08-12 22:19:43 -0300

    test: ensure LoginController returns status 400 if email isnt provided

--- src/presentation/controllers/login/index.spec.ts
+++ src/presentation/controllers/login/index.spec.ts
@@ -1,11 +1,32 @@
 import { LoginController } from '@/presentation/controllers/login'
 
+interface SutTypes {
+  sut: LoginController
+}
+
+function makeSut(): SutTypes {
+  const sut = new LoginController()
+  return { sut }
+}
+
 describe('LoginController', () => {
   it('should be defined', () => {
-    expect(new LoginController()).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut).toBeDefined()
   })
 
   it('should have a method called handle', () => {
-    expect(new LoginController().handle).toBeDefined()
+    const { sut } = makeSut()
+
+    expect(sut.handle).toBeDefined()
+  })
+
+  it('should return status 400 if email isnt provided', () => {
+    const { sut } = makeSut()
+
+    const httpResponse = sut.handle({} as any)
+
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
