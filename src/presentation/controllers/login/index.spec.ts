22684289bfa3c61bf4ff0f23f4abd132f6512476
Rafhael Gomes <rafhael@protonmail.com>
2022-08-12 22:11:54 -0300

    test: ensure LoginController is defined

---
+++ src/presentation/controllers/login/index.spec.ts
@@ -0,0 +1,7 @@
+import { LoginController } from '@/presentation/controllers/login'
+
+describe('LoginController', () => {
+  it('should be defined', () => {
+    expect(new LoginController()).toBeDefined()
+  })
+})
