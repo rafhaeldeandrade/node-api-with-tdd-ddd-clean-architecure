3b0bc66382f8050b879184b0bca93eaa8f6edfc2
Rafhael Gomes <rafhael@protonmail.com>
2022-08-14 14:56:25 -0300

    test: ensure ValidationComposite returns null on success

--- src/presentation/helpers/validation-composite.spec.ts
+++ src/presentation/helpers/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { MissingParamError } from '../errors/missing-param-error'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
@@ -14,21 +15,32 @@ const fakeValidators = [
   new ValidationStub('field2')
 ]
 
+interface SutTypes {
+  sut: ValidationComposite
+}
+
+function makeSut(): SutTypes {
+  const sut = new ValidationComposite(fakeValidators)
+  return {
+    sut
+  }
+}
+
 describe('ValidationComposite', () => {
   it('should be defined', () => {
-    const sut = new ValidationComposite(fakeValidators)
+    const { sut } = makeSut()
 
     expect(sut).toBeDefined()
   })
 
   it('should have a method called validate', () => {
-    const sut = new ValidationComposite(fakeValidators)
+    const { sut } = makeSut()
 
     expect(sut.validate).toBeDefined()
   })
 
   it('should return an error if validate fails', () => {
-    const sut = new ValidationComposite(fakeValidators)
+    const { sut } = makeSut()
 
     jest
       .spyOn(ValidationStub.prototype, 'validate')
@@ -37,5 +49,16 @@ describe('ValidationComposite', () => {
     const result = sut.validate({})
 
     expect(result).toEqual(new MissingParamError('field1'))
+  })
+
+  it('should return null on success', () => {
+    const { sut } = makeSut()
+
+    const result = sut.validate({
+      field1: faker.random.word(),
+      field2: faker.random.word()
+    })
+
+    expect(result).toBe(null)
   })
 })
