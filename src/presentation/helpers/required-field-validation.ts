12a1dbba51661cccde7a877fe9526ba4a2463298
Rafhael Gomes <rafhael@protonmail.com>
2022-08-14 14:39:51 -0300

    feat: ensure RequiredFieldValidation returns MissingParamError if fieldName isnt in input

--- src/presentation/helpers/required-field-validation.ts
+++ src/presentation/helpers/required-field-validation.ts
@@ -1,5 +1,11 @@
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+
 export class RequiredFieldValidation {
+  constructor(private readonly fieldName: string) {}
   validate(input: any): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
     return null
   }
 }
