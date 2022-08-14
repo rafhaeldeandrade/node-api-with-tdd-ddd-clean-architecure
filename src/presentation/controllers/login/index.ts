4dcc99f3a97ab9b3c9aa4bc8e2a7566796b92371
Rafhael Gomes <rafhael@protonmail.com>
2022-08-13 23:40:29 -0300

    feat: ensure LoginController calls authentication with the correct params

--- src/presentation/controllers/login/index.ts
+++ src/presentation/controllers/login/index.ts
@@ -1,18 +1,25 @@
+import { Authentication } from '@/domain/usecases/authentication'
+import { Controller } from '@/presentation/contracts/controller'
 import { EmailValidator } from '@/presentation/contracts/email-validator'
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 
-export class LoginController {
-  constructor(private readonly emailValidator: EmailValidator) {}
-  handle(httpRequest: httpRequest): httpResponse {
+export class LoginController implements Controller {
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly authentication: Authentication
+  ) {}
+
+  async handle(httpRequest: httpRequest): Promise<httpResponse> {
     try {
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!emailIsValid) return badRequest(new InvalidParamError('email'))
       const { email, password } = httpRequest.body
       if (!email) return badRequest(new MissingParamError('email'))
       if (!password) return badRequest(new MissingParamError('password'))
+      await this.authentication.auth({ email, password })
       return {
         statusCode: 200,
         body: {}
