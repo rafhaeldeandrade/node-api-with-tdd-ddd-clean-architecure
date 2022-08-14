4b26df7ea85778f70b808242523263f5e40c3f4f
Rafhael Gomes <rafhael@protonmail.com>
2022-08-14 14:54:22 -0300

    test: ensure ValidationComposite return an error if validate fails

--- src/presentation/helpers/validation-composite.spec.ts
+++ src/presentation/helpers/validation-composite.spec.ts
@@ -1,15 +1,41 @@
+import { MissingParamError } from '../errors/missing-param-error'
+import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
+
+class ValidationStub implements Validation {
+  constructor(private readonly fieldName: string) {}
+  validate(input: any): Error | null {
+    return null
+  }
+}
+
+const fakeValidators = [
+  new ValidationStub('field1'),
+  new ValidationStub('field2')
+]
 
 describe('ValidationComposite', () => {
   it('should be defined', () => {
-    const sut = new ValidationComposite()
+    const sut = new ValidationComposite(fakeValidators)
 
     expect(sut).toBeDefined()
   })
 
   it('should have a method called validate', () => {
-    const sut = new ValidationComposite()
+    const sut = new ValidationComposite(fakeValidators)
 
     expect(sut.validate).toBeDefined()
+  })
+
+  it('should return an error if validate fails', () => {
+    const sut = new ValidationComposite(fakeValidators)
+
+    jest
+      .spyOn(ValidationStub.prototype, 'validate')
+      .mockImplementationOnce(() => new MissingParamError('field1'))
+
+    const result = sut.validate({})
+
+    expect(result).toEqual(new MissingParamError('field1'))
   })
 })
