097818b97229ba1064a3d68aa4fe2496f095e696
Rafhael Gomes <rafhael@protonmail.com>
2022-08-13 23:28:08 -0300

    feat: ensure LoginController returns 500 if emailValidator throws

--- src/presentation/controllers/login/index.ts
+++ src/presentation/controllers/login/index.ts
@@ -2,20 +2,23 @@ import { EmailValidator } from '@/presentation/contracts/email-validator'
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-import { badRequest } from '@/presentation/helpers/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 
-/* eslint-disable @typescript-eslint/no-extraneous-class */
 export class LoginController {
   constructor(private readonly emailValidator: EmailValidator) {}
   handle(httpRequest: httpRequest): httpResponse {
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!emailIsValid) return badRequest(new InvalidParamError('email'))
-    const { email, password } = httpRequest.body
-    if (!email) return badRequest(new MissingParamError('email'))
-    if (!password) return badRequest(new MissingParamError('password'))
-    return {
-      statusCode: 200,
-      body: {}
+    try {
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!emailIsValid) return badRequest(new InvalidParamError('email'))
+      const { email, password } = httpRequest.body
+      if (!email) return badRequest(new MissingParamError('email'))
+      if (!password) return badRequest(new MissingParamError('password'))
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (error) {
+      return serverError(error as Error)
     }
   }
 }
