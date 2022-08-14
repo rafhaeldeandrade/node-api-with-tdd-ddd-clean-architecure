98c89de0ccf830a04501ca9eeae2c0b47f3048e7
Rafhael Gomes <rafhael@protonmail.com>
2022-08-14 14:54:10 -0300

    feat: ensure ValidationComposite return an error if validate fails

--- src/presentation/helpers/validation-composite.ts
+++ src/presentation/helpers/validation-composite.ts
@@ -1,5 +1,14 @@
+import { Validation } from '@/presentation/helpers/validation'
+
 export class ValidationComposite {
+  constructor(private readonly validators: Validation[]) {}
   validate(input: any): Error | null {
+    for (const validator of this.validators) {
+      const error = validator.validate(input)
+      if (error) {
+        return error
+      }
+    }
     return null
   }
 }
