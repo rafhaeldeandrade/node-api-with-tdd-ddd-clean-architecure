b2738e731b95c4074e1358b03e6a14fcff567b03
Rafhael Gomes <rafhael@protonmail.com>
2022-08-14 14:40:15 -0300

    test: ensure RequiredFieldValidation returns MissingParamError if fieldName isnt in input

--- src/presentation/helpers/required-field-validation.spec.ts
+++ src/presentation/helpers/required-field-validation.spec.ts
@@ -1,15 +1,27 @@
+import { faker } from '@faker-js/faker'
 import { RequiredFieldValidation } from '@/presentation/helpers/required-field-validation'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+
+const fakeFieldName = faker.random.word()
 
 describe('RequiredFieldValidation', () => {
   it('should be defined', () => {
-    const sut = new RequiredFieldValidation()
+    const sut = new RequiredFieldValidation(fakeFieldName)
 
     expect(sut).toBeDefined()
   })
 
   it('should have a method called validate', () => {
-    const sut = new RequiredFieldValidation()
+    const sut = new RequiredFieldValidation(fakeFieldName)
 
     expect(sut.validate).toBeDefined()
+  })
+
+  it('should return MissingParamError if fieldName isnt in input', () => {
+    const sut = new RequiredFieldValidation(fakeFieldName)
+
+    const error = sut.validate({})
+
+    expect(error).toEqual(new MissingParamError(fakeFieldName))
   })
 })
