2455f48e2d21b84837e0f7f704d8370e53366ef5
Rafhael Gomes <rafhael@protonmail.com>
2022-08-16 20:10:16 -0300

    test: ensure CompareFieldValidation returns InvalidParamError if fieldToCompareName's value isnt equal to fieldName's value

--- src/presentation/helpers/compare-field-validation.spec.ts
+++ src/presentation/helpers/compare-field-validation.spec.ts
@@ -1,26 +1,41 @@
 import { CompareFieldValidation } from '@/presentation/helpers/compare-field-validdation'
+import { faker } from '@faker-js/faker'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 interface SutTypes {
   sut: CompareFieldValidation
 }
 
-function makeSut(): SutTypes {
-  const sut = new CompareFieldValidation()
+function makeSut(fieldName: string, fieldToCompareName: string): SutTypes {
+  const sut = new CompareFieldValidation(fieldName, fieldToCompareName)
   return {
     sut
   }
 }
+const fakeFieldName = faker.random.word()
+const fakeToCompareName = faker.random.word()
 
 describe('CompareFieldValidation', () => {
   it('should be defined', () => {
-    const { sut } = makeSut()
+    const { sut } = makeSut(fakeFieldName, fakeToCompareName)
 
     expect(sut).toBeDefined()
   })
 
   it('should have a method called validate', () => {
-    const { sut } = makeSut()
+    const { sut } = makeSut(fakeFieldName, fakeToCompareName)
 
     expect(sut.validate).toBeDefined()
+  })
+
+  it('should return InvalidParamError if fieldToCompareName isnt the same as fieldName', () => {
+    const { sut } = makeSut(fakeFieldName, fakeToCompareName)
+
+    const error = sut.validate({
+      [fakeFieldName]: 1,
+      [fakeToCompareName]: 2
+    })
+
+    expect(error).toEqual(new InvalidParamError(fakeToCompareName))
   })
 })
