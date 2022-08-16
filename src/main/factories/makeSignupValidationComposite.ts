47b06990e983a9cf0ba15fefbef651ff02a83bfe
Rafhael Gomes <rafhael@protonmail.com>
2022-08-16 20:16:17 -0300

    feat: ensure makeSignupValidationComposite is providing CompareFieldValidation with password and passwordConfirmation to ValidationComposite

--- src/main/factories/makeSignupValidationComposite.ts
+++ src/main/factories/makeSignupValidationComposite.ts
@@ -1,3 +1,4 @@
+import { CompareFieldValidation } from '@/presentation/helpers/compare-field-validdation'
 import { RequiredFieldValidation } from '@/presentation/helpers/required-field-validation'
 import { ValidationComposite } from '@/presentation/helpers/validation-composite'
 
@@ -7,5 +8,8 @@ export function makeSignupValidationComposite(): ValidationComposite {
   for (const param of requiredParams) {
     validations.push(new RequiredFieldValidation(param))
   }
+  validations.push(
+    new CompareFieldValidation('password', 'passwordConfirmation')
+  )
   return new ValidationComposite(validations)
 }
