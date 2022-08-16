5034c6eef92ad76a4b19c364439c5afee53cadc6
Rafhael Gomes <rafhael@protonmail.com>
2022-08-16 20:10:01 -0300

    feat: ensure CompareFieldValidation returns InvalidParamError if fieldToCompareName's value isnt equal to fieldName's value

--- src/presentation/helpers/compare-field-validdation.ts
+++ src/presentation/helpers/compare-field-validdation.ts
@@ -1,6 +1,14 @@
 import { Validation } from '@/presentation/helpers/validation'
+import { InvalidParamError } from '../errors/invalid-param-error'
 export class CompareFieldValidation implements Validation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
+
   validate(input: any): Error | null {
-    return null
+    if (input[this.fieldName] === input[this.fieldToCompareName]) return null
+
+    return new InvalidParamError(this.fieldToCompareName)
   }
 }
