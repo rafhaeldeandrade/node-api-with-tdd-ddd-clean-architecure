3bbd64c9a3a24f419c4feda0870b1034d7ff9bbe
Rafhael Gomes <rafhael@protonmail.com>
2022-08-16 20:38:08 -0300

    test: ensure makeSignupValidationComposite is providing EMailValidation with the correct field to ValidateComposite

--- src/main/factories/makeSignupValidationComposite.spec.ts
+++ src/main/factories/makeSignupValidationComposite.spec.ts
@@ -1,7 +1,9 @@
 import { CompareFieldValidation } from '@/presentation/helpers/compare-field-validdation'
+import { EmailValidation } from '@/presentation/helpers/email-validation'
 import { RequiredFieldValidation } from '@/presentation/helpers/required-field-validation'
 import { ValidationComposite } from '@/presentation/helpers/validation-composite'
-import { makeSignupValidationComposite } from './makeSignupValidationComposite'
+import { EmailValidatorAdapter } from '@/utils/email-validator'
+import { makeSignupValidationComposite } from '@/main/factories/makeSignupValidationComposite'
 jest.mock('@/presentation/helpers/validation-composite', () => {
   return {
     ValidationComposite: jest.fn().mockImplementation(() => {
@@ -23,7 +25,8 @@ describe('SignupValidationComposite', () => {
     validations.push(
       new CompareFieldValidation('password', 'passwordConfirmation')
     )
-
+    const emailValidator = new EmailValidatorAdapter()
+    validations.push(new EmailValidation('email', emailValidator))
     const validationComposite = ValidationComposite as jest.Mock
 
     expect(validationComposite.mock.calls.length).toBe(1)
