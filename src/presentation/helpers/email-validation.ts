3863d9d8b36d4996a98ff5507058fe2fadcc5c58
Rafhael Gomes <rafhael@protonmail.com>
2022-08-16 20:30:09 -0300

    feat: ensure EmailValidation calls EmailValidator with the correct email

--- src/presentation/helpers/email-validation.ts
+++ src/presentation/helpers/email-validation.ts
@@ -1,7 +1,14 @@
 import { Validation } from '@/presentation/helpers/validation'
+import { EmailValidator } from '@/presentation/contracts/email-validator'
 
 export class EmailValidation implements Validation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   validate(input: any): Error | null {
+    this.emailValidator.isValid(input[this.fieldName])
     return null
   }
 }
