d153a38275d2677cf5ddc0ed7f194743275a5f14
Rafhael Gomes <rafhael@protonmail.com>
2022-08-17 21:40:18 -0300

    refactor: fix imports in signup route

--- src/main/routes/signup.routes.ts
+++ src/main/routes/signup.routes.ts
@@ -1,6 +1,6 @@
 import { Router, RequestHandler } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { makeSignupController } from '@/main/factories/makeSignupController'
+import { makeSignupController } from '@/main/factories/signup/makeSignupController'
 
 export default function (router: Router): void {
   router.post('/signup', adaptRoute(makeSignupController()) as RequestHandler)
