5883cae32ead4e66d760dc92a35df59d470fe6db
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 00:51:35 -0300

    test: ensure DbAuthentication calls loadAccountByEmailRepository with the correct param

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,12 +1,38 @@
+import { LoadAccountByEmailRepository } from '@/data/contracts/db/db-load-account-by-email-repository'
 import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
+import { AccountModel } from '@/domain/models/account'
+import { faker } from '@faker-js/faker'
+
+const fakePassword = faker.internet.password()
+const fakeAccount = {
+  id: faker.datatype.uuid(),
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: fakePassword
+}
+class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+  async load(email: string): Promise<AccountModel> {
+    return fakeAccount
+  }
+}
 
 interface SutTypes {
   sut: DbAuthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
 }
 function makeSut(): SutTypes {
+  const loadAccountByEmailRepositoryStub =
+    new LoadAccountByEmailRepositoryStub()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
   return {
-    sut: new DbAuthentication()
+    sut,
+    loadAccountByEmailRepositoryStub
   }
+}
+
+const fakeParams = {
+  email: faker.internet.email(),
+  password: faker.internet.password()
 }
 
 describe('DbAuthentication', () => {
@@ -19,5 +45,15 @@ describe('DbAuthentication', () => {
     const { sut } = makeSut()
 
     expect(sut.auth).toBeDefined()
+  })
+
+  it('should call loadAccountByEmailRepository with the correct param', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+
+    await sut.auth(fakeParams)
+
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(fakeParams.email)
   })
 })
