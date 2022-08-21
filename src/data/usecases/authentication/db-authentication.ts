23039d94c265ff7bfe5b419ef4d36a48db9db3fc
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 00:42:13 -0300

    feat: ensure DbAuthentication has a method called auth

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class DbAuthentication {}
+import { Authentication } from '@/domain/usecases/authentication'
+
+export class DbAuthentication implements Authentication {
+  async auth(): Promise<string> {
+    return ''
+  }
+}
