27b0f6f058ff95690ec93f95d5c1645463ac5cb1
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 21:34:24 -0300

    feat: ensure Argon2Adapter has a method called compare

--- src/infra/cryptography/argon2-adapter.ts
+++ src/infra/cryptography/argon2-adapter.ts
@@ -1,14 +1,17 @@
+import { HashComparer } from '@/data/contracts/authentication/hash-comparer'
 import { Hasher } from '@/data/contracts/authentication/hasher'
 import argon2 from 'argon2'
 
-export class Argon2Adapter implements Hasher {
+export class Argon2Adapter implements Hasher, HashComparer {
   constructor(
     private readonly argon2Options?: argon2.Options & { raw?: false }
   ) {}
 
   async hash(value: string): Promise<string> {
-    const hash = await argon2.hash(value, this.argon2Options)
+    return await argon2.hash(value, this.argon2Options)
+  }
 
-    return hash
+  async compare(value: string, hash: string): Promise<boolean> {
+    return await argon2.verify(hash, value, this.argon2Options)
   }
 }
