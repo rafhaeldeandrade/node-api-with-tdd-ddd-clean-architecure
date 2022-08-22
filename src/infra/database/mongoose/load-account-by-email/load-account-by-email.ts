1aefb8106cc06de522106132573bbceee85319a4
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 21:03:30 -0300

    feat: ensure MongooseLoadAccountByEmail has a method called load

--- src/infra/database/mongoose/load-account-by-email/load-account-by-email.ts
+++ src/infra/database/mongoose/load-account-by-email/load-account-by-email.ts
@@ -1,2 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class MongooseLoadAccountByEmail {}
+export class MongooseLoadAccountByEmail {
+  load(): any {
+    return null
+  }
+}
