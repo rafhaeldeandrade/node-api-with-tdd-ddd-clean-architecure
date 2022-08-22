c4a35d8755353da82828af76084e2be0a599cbf4
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 21:15:53 -0300

    test: ensure MongooseLoadAccountByEmail calls mongooseAccountModel with the correct param

--- src/infra/database/mongoose/load-account-by-email/load-account-by-email.spec.ts
+++ src/infra/database/mongoose/load-account-by-email/load-account-by-email.spec.ts
@@ -1,4 +1,6 @@
 import { MongooseLoadAccountByEmail } from '@/infra/database/mongoose/load-account-by-email/load-account-by-email'
+import { mongooseAccountModel } from '@/infra/database/mongoose/schemas/account'
+import { faker } from '@faker-js/faker'
 
 interface SutTypes {
   sut: MongooseLoadAccountByEmail
@@ -10,6 +12,9 @@ function makeSut(): SutTypes {
   }
 }
 
+const fakeParams = {
+  email: faker.internet.email()
+}
 describe('MongooseLoadAccountByEmailRepository', () => {
   it('should be defined', () => {
     const { sut } = makeSut()
@@ -19,5 +24,18 @@ describe('MongooseLoadAccountByEmailRepository', () => {
   it('should have a method called load', () => {
     const { sut } = makeSut()
     expect(sut.load).toBeDefined()
+  })
+
+  it('should call mongooseAccountModel with the correct param', async () => {
+    const { sut } = makeSut()
+    const findOneSpy = jest
+      .spyOn(mongooseAccountModel, 'findOne')
+      .mockResolvedValueOnce(null as any)
+
+    await sut.load(fakeParams.email)
+
+    expect(findOneSpy).toHaveBeenCalledWith({ email: fakeParams.email }, null, {
+      lean: true
+    })
   })
 })
