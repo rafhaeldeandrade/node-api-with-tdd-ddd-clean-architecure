695241d295af6a50ab0125a0224031a8f58d7233
Rafhael Gomes <rafhael@protonmail.com>
2022-08-21 21:03:42 -0300

    test: ensure MongooseLoadAccountByEmail has a method called load

--- src/infra/database/mongoose/load-account-by-email/load-account-by-email.spec.ts
+++ src/infra/database/mongoose/load-account-by-email/load-account-by-email.spec.ts
@@ -1,8 +1,23 @@
 import { MongooseLoadAccountByEmail } from '@/infra/database/mongoose/load-account-by-email/load-account-by-email'
 
+interface SutTypes {
+  sut: MongooseLoadAccountByEmail
+}
+
+function makeSut(): SutTypes {
+  return {
+    sut: new MongooseLoadAccountByEmail()
+  }
+}
+
 describe('MongooseLoadAccountByEmailRepository', () => {
   it('should be defined', () => {
-    const sut = new MongooseLoadAccountByEmail()
+    const { sut } = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('should have a method called load', () => {
+    const { sut } = makeSut()
+    expect(sut.load).toBeDefined()
   })
 })
