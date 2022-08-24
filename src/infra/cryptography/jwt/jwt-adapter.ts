19200eee4e2a3dc1d6cfe8d7e0e545639908b5bc
Rafhael Gomes <rafhael@protonmail.com>
2022-08-23 21:32:09 -0300

    feat: ensure JwtAdapter calls jwt.sign with correct params when encrypt method is invoked

--- src/infra/cryptography/jwt/jwt-adapter.ts
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -1,7 +1,9 @@
 import { Encrypter } from '@/data/contracts/authentication/encrypter'
+import jwt from 'jsonwebtoken'
 
 export class JwtAdapter implements Encrypter {
+  constructor(private readonly jwtSecret: string) {}
   async encrypt(value: string): Promise<string> {
-    return null as unknown as string
+    return jwt.sign(value, this.jwtSecret)
   }
 }
