a0303f13cb29ae27e2f0d679119d6f03a8a01054
Rafhael Gomes <rafhael@protonmail.com>
2022-08-23 21:22:34 -0300

    test: ensure JwtAdapter has a method called encryp

--- src/infra/cryptography/jwt/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt/jwt-adapter.spec.ts
@@ -1,7 +1,23 @@
 import { JwtAdapter } from '@/infra/cryptography/jwt/jwt-adapter'
 
+interface SutTypes {
+  sut: JwtAdapter
+}
+function makeSut(): SutTypes {
+  const sut = new JwtAdapter()
+  return {
+    sut
+  }
+}
+
 describe('JwtAdapter', () => {
   it('should be defined', () => {
-    expect(new JwtAdapter()).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut).toBeDefined()
+  })
+
+  it('should have a method called encrypt', () => {
+    const { sut } = makeSut()
+    expect(sut.encrypt).toBeDefined()
   })
 })
