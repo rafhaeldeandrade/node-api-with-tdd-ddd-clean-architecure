4b10556099e02e6184374fdde86fa6f05104b55a
Rafhael Gomes <rafhael@protonmail.com>
2022-08-23 21:32:26 -0300

    test: ensure JwtAdapter calls jwt.sign with correct params when encrypt method is invoked

--- src/infra/cryptography/jwt/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt/jwt-adapter.spec.ts
@@ -1,10 +1,14 @@
+import jwt from 'jsonwebtoken'
 import { JwtAdapter } from '@/infra/cryptography/jwt/jwt-adapter'
+import { faker } from '@faker-js/faker'
 
+const fakeValue = faker.datatype.uuid()
+const fakeJwtSecret = faker.datatype.string(128)
 interface SutTypes {
   sut: JwtAdapter
 }
 function makeSut(): SutTypes {
-  const sut = new JwtAdapter()
+  const sut = new JwtAdapter(fakeJwtSecret)
   return {
     sut
   }
@@ -19,5 +23,12 @@ describe('JwtAdapter', () => {
   it('should have a method called encrypt', () => {
     const { sut } = makeSut()
     expect(sut.encrypt).toBeDefined()
+  })
+
+  it('should call jwt.sign with correct params when encrypt method is invoked', async () => {
+    const { sut } = makeSut()
+    const jwtSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt(fakeValue)
+    expect(jwtSpy).toHaveBeenCalledWith(fakeValue, fakeJwtSecret)
   })
 })
