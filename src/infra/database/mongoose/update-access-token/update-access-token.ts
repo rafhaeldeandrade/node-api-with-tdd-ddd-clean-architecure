90cec935b51f4c022f062865ff26945f2b920415
Rafhael Gomes <rafhael@protonmail.com>
2022-08-23 21:47:37 -0300

    feat: ensure MongooseUpdateAccessTokenRepository has a method called update

--- src/infra/database/mongoose/update-access-token/update-access-token.ts
+++ src/infra/database/mongoose/update-access-token/update-access-token.ts
@@ -1,2 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class MongooseUpdateAccessToken {}
+export class MongooseUpdateAccessToken {
+  update(): any {
+    return null
+  }
+}
