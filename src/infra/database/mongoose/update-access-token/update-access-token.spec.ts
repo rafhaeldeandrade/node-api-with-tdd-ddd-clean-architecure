121eea22f828b0237524933e4dad061dcc552a21
Rafhael Gomes <rafhael@protonmail.com>
2022-08-23 22:02:21 -0300

    test: ensure MongooseUpdateAccessTokenRepository calls mongooseAccountModel.updateOne with correct params

--- src/infra/database/mongoose/update-access-token/update-access-token.spec.ts
+++ src/infra/database/mongoose/update-access-token/update-access-token.spec.ts
@@ -1,5 +1,11 @@
 import { MongooseUpdateAccessToken } from '@/infra/database/mongoose/update-access-token/update-access-token'
+import { mongooseAccountModel } from '@/infra/database/mongoose/schemas/account'
+import { faker } from '@faker-js/faker'
 
+const fakeParams = {
+  accountId: faker.datatype.uuid(),
+  accessToken: faker.datatype.string(80)
+}
 describe('MongooseUpdateAccessTokenRepository', () => {
   it('should be defined', () => {
     expect(new MongooseUpdateAccessToken()).toBeDefined()
@@ -7,5 +13,19 @@ describe('MongooseUpdateAccessTokenRepository', () => {
 
   it('should have a method called update', () => {
     expect(new MongooseUpdateAccessToken().update).toBeDefined()
+  })
+
+  it('should call mongooseAccountModel.updateOne with correct params', async () => {
+    const sut = new MongooseUpdateAccessToken()
+    const updateOneSpy = jest
+      .spyOn(mongooseAccountModel, 'updateOne')
+      .mockResolvedValueOnce(null as any)
+    const { accountId, accessToken } = fakeParams
+    await sut.update(accountId, accessToken)
+    expect(updateOneSpy).toHaveBeenCalledTimes(1)
+    expect(updateOneSpy).toHaveBeenCalledWith(
+      { _id: accountId },
+      { $set: { accessToken } }
+    )
   })
 })
