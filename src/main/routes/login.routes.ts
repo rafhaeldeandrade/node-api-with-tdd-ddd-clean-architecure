c8ed9a6c89875097125980c8e7b3253ef123a04f
Rafhael Gomes <rafhael@protonmail.com>
2022-08-24 22:25:10 -0300

    feat: implements POST /login route

--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,7 +1,9 @@
 import { Router, RequestHandler } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeSignupController } from '@/main/factories/signup/makeSignupController'
+import { makeLoginController } from '@/main/factories/login/makeLoginController'
 
 export default function (router: Router): void {
   router.post('/signup', adaptRoute(makeSignupController()) as RequestHandler)
+  router.post('/login', adaptRoute(makeLoginController()) as RequestHandler)
 }
