1a1360f3965799d5d9491efeccae3871e574ba1d
Rafhael Gomes <rafhael@protonmail.com>
2022-09-03 14:42:58 -0300

    feat: ensure DbAddAccount calls updateAccessTokenRepository with correct params

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -4,13 +4,15 @@ import { AccountModel } from '@/domain/models/account'
 import { AddAccount, AddAccountModel } from '@/domain/usecases/add-account'
 import { LoadAccountByEmailRepository } from '@/data/contracts/database/load-account-by-email-repository'
 import { Encrypter } from '@/data/contracts/authentication/encrypter'
+import { UpdateAccessTokenRepository } from '@/data/contracts/database/update-access-token-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel | null> {
@@ -23,7 +25,11 @@ export class DbAddAccount implements AddAccount {
       ...accountData,
       password: hashedPassword
     })
-    this.encrypter.encrypt(createdAccount.id)
+    const accessToken = this.encrypter.encrypt(createdAccount.id)
+    await this.updateAccessTokenRepository.update(
+      createdAccount.id,
+      accessToken
+    )
     return createdAccount
   }
 }
