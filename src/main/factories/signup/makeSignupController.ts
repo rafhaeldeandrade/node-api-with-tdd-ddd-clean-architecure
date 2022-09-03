a945b56c16d0a52ebe0e34fdfa7937770ffc52ab
Rafhael Gomes <rafhael@protonmail.com>
2022-09-03 15:01:44 -0300

    refactor: inject new instances to SignupController factory

--- src/main/factories/signup/makeSignupController.ts
+++ src/main/factories/signup/makeSignupController.ts
@@ -8,15 +8,21 @@ import { Controller } from '@/presentation/contracts/controller'
 import { makeSignupValidationComposite } from '@/main/factories/signup/makeSignupValidationComposite'
 import env from '@/main/config/env'
 import { MongooseLoadAccountByEmail } from '@/infra/database/mongoose/load-account-by-email/load-account-by-email'
+import { JwtAdapter } from '@/infra/cryptography/jwt/jwt-adapter'
+import { MongooseUpdateAccessToken } from '@/infra/database/mongoose/update-access-token/update-access-token'
 
 export function makeSignupController(): Controller {
   const loadAccountByEmail = new MongooseLoadAccountByEmail()
   const argon2Adapter = new Argon2Adapter(env.argon2Options)
   const mongooseAddAccount = new MongooseAddAccount()
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const mongooseUpdateAccessToken = new MongooseUpdateAccessToken()
   const dbAddAccount = new DbAddAccount(
     loadAccountByEmail,
     argon2Adapter,
-    mongooseAddAccount
+    mongooseAddAccount,
+    jwtAdapter,
+    mongooseUpdateAccessToken
   )
   const mongooseLogError = new MongooseLogError()
   const signupController = new SignupController(
