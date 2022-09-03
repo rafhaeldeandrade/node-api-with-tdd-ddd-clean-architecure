ee0b2d385b6ab02dffcf3c3924b2a73523606b37
Rafhael Gomes <rafhael@protonmail.com>
2022-09-02 22:39:03 -0300

    feat: ensure DbAddAccountUseCase calls loadAccountByEmailRepository with correct param

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,14 +2,17 @@ import { AddAccountRepository } from '@/data/contracts/database/add-account-repo
 import { Hasher } from '@/data/contracts/authentication/hasher'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccount, AddAccountModel } from '@/domain/usecases/add-account'
+import { LoadAccountByEmailRepository } from '@/data/contracts/database/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.load(accountData.email)
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add({
       ...accountData,
