0ac4eb56d8b1b29226c7da7a69b1902d7d54d9cd
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 14:56:42 -0300

    feat: ensure MongooseLoadPostByTitle returns a post on success

--- src/infra/database/mongoose/load-post-by-title.ts
+++ src/infra/database/mongoose/load-post-by-title.ts
@@ -4,7 +4,21 @@ import { mongoosePostModel } from '@/infra/database/mongoose/schemas/post'
 
 export class MongooseLoadPostByTitle implements LoadPostByTitleRepository {
   async load(postTitle: string): Promise<PostModel | null> {
-    await mongoosePostModel.findOne({ title: postTitle }, null, { lean: true })
-    return null
+    const savedPost = await mongoosePostModel.findOne(
+      { title: postTitle },
+      null,
+      { lean: true }
+    )
+    if (!savedPost) return null
+    return {
+      id: savedPost._id.toString(),
+      title: savedPost.title,
+      subtitle: savedPost.subtitle,
+      postDate: savedPost.postDate,
+      categories: savedPost.categories,
+      authorId: savedPost.authorId,
+      post: savedPost.post,
+      urlSlug: savedPost.urlSlug
+    }
   }
 }
