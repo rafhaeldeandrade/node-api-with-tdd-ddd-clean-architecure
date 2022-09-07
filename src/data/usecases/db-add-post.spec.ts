96b47b4ccfb071e5989553980b5cc30699fd00a0
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 11:41:48 -0300

    test: ensure DbAddPost calls generateUrlSlug.generate with correct param when loadPostByTitleRepository dont return a post

--- src/data/usecases/db-add-post.spec.ts
+++ src/data/usecases/db-add-post.spec.ts
@@ -2,6 +2,7 @@ import { DbAddPost } from '@/data/usecases/db-add-post'
 import { LoadPostByTitleRepository } from '@/data/contracts/database/load-post-by-title-repository'
 import { PostModel } from '@/domain/models/post'
 import { faker } from '@faker-js/faker'
+import { GenerateUrlSlug } from '@/data/contracts/utils/generate-url-slug'
 
 class LoadPostByTitleRepositoryStub implements LoadPostByTitleRepository {
   async load(title: string): Promise<PostModel | null> {
@@ -9,17 +10,26 @@ class LoadPostByTitleRepositoryStub implements LoadPostByTitleRepository {
   }
 }
 
+class GenerateUrlSlugStub implements GenerateUrlSlug {
+  generate(title: string): string {
+    return faker.lorem.slug()
+  }
+}
+
 interface SutTypes {
   sut: DbAddPost
   loadPostByTitleRepositoryStub: LoadPostByTitleRepository
+  generateUrlSlugStub: GenerateUrlSlug
 }
 
 function makeSut(): SutTypes {
   const loadPostByTitleRepositoryStub = new LoadPostByTitleRepositoryStub()
-  const sut = new DbAddPost(loadPostByTitleRepositoryStub)
+  const generateUrlSlugStub = new GenerateUrlSlugStub()
+  const sut = new DbAddPost(loadPostByTitleRepositoryStub, generateUrlSlugStub)
   return {
     sut,
-    loadPostByTitleRepositoryStub
+    loadPostByTitleRepositoryStub,
+    generateUrlSlugStub
   }
 }
 
@@ -53,5 +63,27 @@ describe('AddPost Usecase', () => {
 
     expect(loadSpy).toHaveBeenCalledTimes(1)
     expect(loadSpy).toHaveBeenCalledWith(fakeParams.title)
+  })
+
+  it('should return null if loadPostByTitleRepository returns a previously created post', async () => {
+    const { sut, loadPostByTitleRepositoryStub } = makeSut()
+    jest.spyOn(loadPostByTitleRepositoryStub, 'load').mockResolvedValueOnce({
+      id: faker.datatype.number().toString(),
+      ...fakeParams
+    })
+
+    const result = await sut.add(fakeParams)
+
+    expect(result).toBeNull()
+  })
+
+  it('should call generateUrlSlug.generate with correct param when loadPostByTitleRepository dont return a post', async () => {
+    const { sut, generateUrlSlugStub } = makeSut()
+    const generateSpy = jest.spyOn(generateUrlSlugStub, 'generate')
+
+    await sut.add(fakeParams)
+
+    expect(generateSpy).toHaveBeenCalledTimes(1)
+    expect(generateSpy).toHaveBeenCalledWith(fakeParams.title)
   })
 })
