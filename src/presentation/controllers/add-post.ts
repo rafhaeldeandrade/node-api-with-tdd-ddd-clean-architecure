b52fac3f8c2525417f6da02176263b4faf1fb6fc
Rafhael Gomes <rafhael@protonmail.com>
2022-09-06 22:38:57 -0300

    feat: ensure AddPostController returns 400 if validation.validate returns an error

--- src/presentation/controllers/add-post.ts
+++ src/presentation/controllers/add-post.ts
@@ -1,12 +1,14 @@
 import { Controller } from '@/presentation/contracts/controller'
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { Validation } from '@/presentation/contracts/validation'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 export class AddPostController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(params: httpRequest): Promise<httpResponse> {
-    this.validation.validate(params.body)
+    const error = this.validation.validate(params.body)
+    if (error) return badRequest(error)
     return '' as unknown as httpResponse
   }
 }
