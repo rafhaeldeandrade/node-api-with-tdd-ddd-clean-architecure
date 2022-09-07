48934786d116148dd136946d97debea9e1156738
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 11:30:06 -0300

    test: ensure DbAddPost calls loadPostByTitleRepository with correct param

--- src/data/usecases/db-add-post.spec.ts
+++ src/data/usecases/db-add-post.spec.ts
@@ -1,14 +1,35 @@
 import { DbAddPost } from '@/data/usecases/db-add-post'
+import { LoadPostByTitleRepository } from '@/data/contracts/database/load-post-by-title-repository'
+import { PostModel } from '@/domain/models/post'
+import { faker } from '@faker-js/faker'
+
+class LoadPostByTitleRepositoryStub implements LoadPostByTitleRepository {
+  async load(title: string): Promise<PostModel | null> {
+    return null
+  }
+}
 
 interface SutTypes {
   sut: DbAddPost
+  loadPostByTitleRepositoryStub: LoadPostByTitleRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new DbAddPost()
+  const loadPostByTitleRepositoryStub = new LoadPostByTitleRepositoryStub()
+  const sut = new DbAddPost(loadPostByTitleRepositoryStub)
   return {
-    sut
+    sut,
+    loadPostByTitleRepositoryStub
   }
+}
+
+const fakeParams = {
+  title: faker.lorem.sentence(),
+  subtitle: faker.lorem.sentence(),
+  postDate: faker.date.past(),
+  categories: [faker.lorem.word()],
+  authorId: faker.datatype.number().toString(),
+  post: faker.lorem.paragraph()
 }
 
 describe('AddPost Usecase', () => {
@@ -22,5 +43,15 @@ describe('AddPost Usecase', () => {
     const { sut } = makeSut()
 
     expect(sut.add).toBeDefined()
+  })
+
+  it('should call loadPostByTitleRepository with correct value', async () => {
+    const { sut, loadPostByTitleRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadPostByTitleRepositoryStub, 'load')
+
+    await sut.add(fakeParams)
+
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(fakeParams.title)
   })
 })
