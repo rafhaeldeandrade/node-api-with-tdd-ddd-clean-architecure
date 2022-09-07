08e4bbcc5588e2dca94dde5b927ba9b4358409c8
Rafhael Gomes <rafhael@protonmail.com>
2022-09-06 22:22:24 -0300

    feat: ensure AddPostController has a method called handle

--- src/presentation/controllers/add-post.ts
+++ src/presentation/controllers/add-post.ts
@@ -1,2 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class AddPostController {}
+import { Controller } from '@/presentation/contracts/controller'
+import { httpRequest, httpResponse } from '@/presentation/contracts/http'
+
+export class AddPostController implements Controller {
+  async handle(params: httpRequest): Promise<httpResponse> {
+    return '' as unknown as httpResponse
+  }
+}
