10e7d8634075062f195912e54cf734ad84c0f215
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 16:36:35 -0300

    feat: ensure MongooseAddPost calls mongoosePostModel with correct params

--- src/infra/database/mongoose/add-post.ts
+++ src/infra/database/mongoose/add-post.ts
@@ -3,9 +3,11 @@ import {
   AddPostRepositoryInput
 } from '@/data/contracts/database/add-post-repository'
 import { PostModel } from '@/domain/models/post'
+import { mongoosePostModel } from './schemas/post'
 
 export class MongooseAddPost implements AddPostRepository {
   async add(params: AddPostRepositoryInput): Promise<PostModel> {
+    await mongoosePostModel.create(params)
     return null as unknown as PostModel
   }
 }
