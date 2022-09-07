f077fe688ad704f576b22884da13e53d07c741b8
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 16:36:51 -0300

    test: ensure MongooseAddPost calls mongoosePostModel with correct params

--- src/infra/database/mongoose/add-post.spec.ts
+++ src/infra/database/mongoose/add-post.spec.ts
@@ -1,5 +1,7 @@
 import { AddPostRepository } from '@/data/contracts/database/add-post-repository'
 import { MongooseAddPost } from '@/infra/database/mongoose/add-post'
+import { faker } from '@faker-js/faker'
+import { mongoosePostModel } from '@/infra/database/mongoose/schemas/post'
 
 interface SutTypes {
   sut: AddPostRepository
@@ -9,6 +11,16 @@ function makeSut(): SutTypes {
   return {
     sut: new MongooseAddPost()
   }
+}
+
+const fakeAddPostInput = {
+  title: faker.lorem.sentence(),
+  subtitle: faker.lorem.sentence(),
+  postDate: faker.date.past(),
+  categories: [faker.lorem.word(), faker.lorem.word()],
+  authorId: faker.datatype.uuid(),
+  post: faker.lorem.paragraph(),
+  urlSlug: faker.lorem.slug()
 }
 
 describe('MongooseAddPostRepository', () => {
@@ -22,5 +34,17 @@ describe('MongooseAddPostRepository', () => {
     const { sut } = makeSut()
 
     expect(sut.add).toBeDefined()
+  })
+
+  it('should call mongoosePostModel with correct params', async () => {
+    const { sut } = makeSut()
+    const createSpy = jest
+      .spyOn(mongoosePostModel, 'create')
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      .mockImplementationOnce(async () => null)
+
+    await sut.add(fakeAddPostInput)
+
+    expect(createSpy).toHaveBeenCalledWith(fakeAddPostInput)
   })
 })
