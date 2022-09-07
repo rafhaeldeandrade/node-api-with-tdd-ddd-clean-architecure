4634279689db10a775957afbf2cb4de2a0b07c05
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 16:25:19 -0300

    test: ensure MongooseAddPost is defined

---
+++ src/infra/database/mongoose/add-post.spec.ts
@@ -0,0 +1,20 @@
+import { AddPostRepository } from '@/data/contracts/database/add-post-repository'
+import { MongooseAddPost } from '@/infra/database/mongoose/add-post'
+
+interface SutTypes {
+  sut: AddPostRepository
+}
+
+function makeSut(): SutTypes {
+  return {
+    sut: new MongooseAddPost()
+  }
+}
+
+describe('MongooseAddPostRepository', () => {
+  it('should be defined', () => {
+    const { sut } = makeSut()
+
+    expect(sut).toBeDefined()
+  })
+})
