4c40be5f1ff8d090612e89fd639a0c664903647a
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 16:16:11 -0300

    feat: ensure GenerateUrlSlug returns a slugfied value

--- src/infra/utils/url-slug/generate-url-slug.ts
+++ src/infra/utils/url-slug/generate-url-slug.ts
@@ -1,8 +1,9 @@
+import * as urlSlug from 'url-slug'
 import { SlugGenerator } from '@/data/contracts/utils/slug-generator'
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class GenerateUrlSlug implements SlugGenerator {
   generate(value: string): string {
-    return value
+    return urlSlug.convert(value)
   }
 }
