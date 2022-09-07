9a7dd531b429e6b4eea4a4777dc02f483482eab7
Rafhael Gomes <rafhael@protonmail.com>
2022-09-07 14:46:44 -0300

    test: ensure MongooseLoadPostByTitle calls mongoosePostModel with correct params

--- src/infra/database/mongoose/load-post-by-title.spec.ts
+++ src/infra/database/mongoose/load-post-by-title.spec.ts
@@ -1,5 +1,7 @@
+import { faker } from '@faker-js/faker'
 import { LoadPostByTitleRepository } from '@/data/contracts/database/load-post-by-title-repository'
-import { MongooseLoadPostByTitle } from './load-post-by-title'
+import { MongooseLoadPostByTitle } from '@/infra/database/mongoose/load-post-by-title'
+import { mongoosePostModel } from '@/infra/database/mongoose/schemas/post'
 
 interface SutTypes {
   sut: LoadPostByTitleRepository
@@ -11,10 +13,30 @@ function makeSut(): SutTypes {
   }
 }
 
+const fakeParam = faker.word.noun()
 describe('MongooseLoadPostByTitleRepository', () => {
   it('should be defined', () => {
     const { sut } = makeSut()
 
     expect(sut).toBeDefined()
+  })
+
+  it('should have a method called load', () => {
+    const { sut } = makeSut()
+
+    expect(sut.load).toBeDefined()
+  })
+
+  it('should call mongoosePostModel with the correct param', async () => {
+    const { sut } = makeSut()
+    const findOneSpy = jest
+      .spyOn(mongoosePostModel, 'findOne')
+      .mockResolvedValueOnce(null as any)
+
+    await sut.load(fakeParam)
+
+    expect(findOneSpy).toHaveBeenCalledWith({ title: fakeParam }, null, {
+      lean: true
+    })
   })
 })
