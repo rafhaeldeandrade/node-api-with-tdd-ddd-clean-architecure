8da4fc2e0a961102d49004948bdb03317dd9aefd
Rafhael Gomes <rafhael@protonmail.com>
2022-09-10 01:59:14 -0300

    fix: body parser receives strict: false because it was throwing when body was provided empty

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/middlewares/body-parser'
 import { corsMiddleware } from '@/main/middlewares/cors'
+import { urlEncoded } from '@/main/middlewares/urlencoded'
 
 export function setupMiddlewares(app: Express): void {
   app.use(bodyParser)
+  app.use(urlEncoded)
   app.use(corsMiddleware)
 }
