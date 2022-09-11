009fc7b2c8808e495c5b07b45ac7dad2c654cb3c
Rafhael Gomes <rafhael@protonmail.com>
2022-09-11 02:29:55 -0300

    feat: ensure AuthenticationMiddleware calls loadAccountByToken.load with correct param

--- src/presentation/middlewares/authentication.ts
+++ src/presentation/middlewares/authentication.ts
@@ -2,12 +2,20 @@ import { Middleware } from '@/presentation/contracts/middleware'
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { SchemaValidation } from '@/presentation/contracts/schema-validation'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
 export class AuthenticationMiddleware implements Middleware {
-  constructor(private readonly validation: SchemaValidation) {}
+  constructor(
+    private readonly validation: SchemaValidation,
+    private readonly loadAccountByTokenUseCase: LoadAccountByToken
+  ) {}
 
   async handle(request: httpRequest): Promise<httpResponse> {
-    const error = await this.validation.validate(request.headers)
+    const { headers } = request
+    const error = await this.validation.validate(headers)
     if (error) return badRequest(error)
+    await this.loadAccountByTokenUseCase.load(
+      headers ? headers['x-access-token'] : ''
+    )
     return null as unknown as httpResponse
   }
 }
