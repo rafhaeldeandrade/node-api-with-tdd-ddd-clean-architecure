2403263f909fee505cc0a7b37e1dfc74f936a175
Rafhael Gomes <rafhael@protonmail.com>
2022-09-11 02:13:32 -0300

    feat: ensure AuthenticationMiddleware returns 400 if validation.validate returns an error

--- src/presentation/middlewares/authentication.ts
+++ src/presentation/middlewares/authentication.ts
@@ -1,11 +1,13 @@
 import { Middleware } from '@/presentation/contracts/middleware'
 import { httpRequest, httpResponse } from '@/presentation/contracts/http'
 import { SchemaValidation } from '@/presentation/contracts/schema-validation'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 export class AuthenticationMiddleware implements Middleware {
   constructor(private readonly validation: SchemaValidation) {}
 
   async handle(request: httpRequest): Promise<httpResponse> {
-    await this.validation.validate(request.headers)
+    const error = await this.validation.validate(request.headers)
+    if (error) return badRequest(error)
     return null as unknown as httpResponse
   }
 }
