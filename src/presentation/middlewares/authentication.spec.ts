9fa1df53fd2a3ac9d1f2a4cfa9805a0644b2fa10
Rafhael Gomes <rafhael@protonmail.com>
2022-09-11 02:11:57 -0300

    test: ensure AuthenticationMiddleware calls validation.validate with correct param

--- src/presentation/middlewares/authentication.spec.ts
+++ src/presentation/middlewares/authentication.spec.ts
@@ -1,12 +1,30 @@
 import { AuthenticationMiddleware } from '@/presentation/middlewares/authentication'
+import { faker } from '@faker-js/faker'
+import { SchemaValidation } from '@/presentation/contracts/schema-validation'
+
+class ValidationStub implements SchemaValidation {
+  async validate(input: any): Promise<Error | null> {
+    return null
+  }
+}
 
 interface SutTypes {
   sut: AuthenticationMiddleware
+  validationStub: SchemaValidation
 }
 
 function makeSut(): SutTypes {
-  const sut = new AuthenticationMiddleware()
-  return { sut }
+  const validationStub = new ValidationStub()
+  const sut = new AuthenticationMiddleware(validationStub)
+  return { sut, validationStub }
+}
+
+const fakeAccessToken = faker.datatype.uuid()
+const fakeHttpRequest = {
+  body: {},
+  headers: {
+    'x-access-token': fakeAccessToken
+  }
 }
 
 describe('Authentication Middleware', () => {
@@ -20,5 +38,15 @@ describe('Authentication Middleware', () => {
     const { sut } = makeSut()
 
     expect(sut.handle).toBeDefined()
+  })
+
+  it('should call validation.validate with correct param', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+
+    await sut.handle(fakeHttpRequest)
+
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith(fakeHttpRequest.headers)
   })
 })
