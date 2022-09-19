cd6b29c69668c2d1d8a0b34aaa2e5b2f87c113e9
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 17:21:06 -0300

    feat: ensure DbLoadAccountByTokenUseCase calls decrypter with correct param

--- src/data/usecases/db-load-account-by-token.ts
+++ src/data/usecases/db-load-account-by-token.ts
@@ -1,13 +1,16 @@
 import { AccountModel } from '@/domain/models/account'
 import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
-import { LoadAccountByTokenRepository } from '../contracts/database/load-account-by-token'
+import { Decrypter } from '@/data/contracts/authentication/decrypter'
+import { LoadAccountByTokenRepository } from '@/data/contracts/database/load-account-by-token'
 
 export class DbLoadAccountByTokenUseCase implements LoadAccountByToken {
   constructor(
+    private readonly decrypter: Decrypter,
     private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
   ) {}
 
   async load(token: string): Promise<AccountModel | null> {
+    await this.decrypter.decrypt(token)
     await this.loadAccountByTokenRepository.load(token)
     return null
   }
