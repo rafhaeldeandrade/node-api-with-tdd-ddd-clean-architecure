17555ecd63a3c6a9dbdd52918bb7a8f39c7b43c7
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 17:39:09 -0300

    feat: ensure JwtAdapter has a method called decrypt

--- src/infra/cryptography/jwt/jwt-adapter.ts
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -1,9 +1,15 @@
+import { Decrypter } from '@/data/contracts/authentication/decrypter'
 import { Encrypter } from '@/data/contracts/authentication/encrypter'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly jwtSecret: string) {}
+
   encrypt(value: string): string {
     return jwt.sign(value, this.jwtSecret)
+  }
+
+  decrypt(value: string): any | null {
+    return jwt.verify(value, this.jwtSecret)
   }
 }
