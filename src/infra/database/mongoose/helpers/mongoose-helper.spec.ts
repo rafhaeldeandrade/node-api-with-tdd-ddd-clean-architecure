5ebab2e6475da9ea9bb725c75690d5e0737d2ed9
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 18:26:31 -0300

    refactor: change mongo url

--- src/infra/database/mongoose/helpers/mongoose-helper.spec.ts
+++ src/infra/database/mongoose/helpers/mongoose-helper.spec.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { mongooseHelper as sut } from '@/infra/database/mongoose/helpers/mongoose-helper'
+import env from '@/main/config/env'
 
 describe('Mongoose Helper', () => {
   afterEach(async () => {
@@ -7,29 +8,21 @@ describe('Mongoose Helper', () => {
   })
 
   it('should connect succesfully', async () => {
-    await sut.connect(
-      'mongodb://user_mongodb_59ea38b9ed:9b49c9d30fcfea691c728c9ef916adc4fdf2bf8d@h1.host.filess.io:27017/mongodb_19b2306cfc'
-    )
+    await sut.connect(env.mongoUrl)
 
     expect(mongoose.connection.readyState).toBe(1)
   })
 
   it('should disconnect succesfully', async () => {
-    await sut.connect(
-      'mongodb://user_mongodb_59ea38b9ed:9b49c9d30fcfea691c728c9ef916adc4fdf2bf8d@h1.host.filess.io:27017/mongodb_19b2306cfc'
-    )
+    await sut.connect(env.mongoUrl)
     await sut.disconnect()
 
     expect(mongoose.connection.readyState).toBe(0)
   })
 
   it('should return undefined when trying to connect if a connection already exists', async () => {
-    await sut.connect(
-      'mongodb://user_mongodb_59ea38b9ed:9b49c9d30fcfea691c728c9ef916adc4fdf2bf8d@h1.host.filess.io:27017/mongodb_19b2306cfc'
-    )
-    const result = await sut.connect(
-      'mongodb://user_mongodb_59ea38b9ed:9b49c9d30fcfea691c728c9ef916adc4fdf2bf8d@h1.host.filess.io:27017/mongodb_19b2306cfc'
-    )
+    await sut.connect(env.mongoUrl)
+    const result = await sut.connect(env.mongoUrl)
 
     expect(result).toBeUndefined()
   })
