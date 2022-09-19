f3d0b8a729c6fea7ff8dcd8b0d49c1cdc1f90a1d
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 17:59:28 -0300

    test: ensure MongooseLoadAccountByToken has a method called load

--- src/infra/database/mongoose/load-account-by-token.spec.ts
+++ src/infra/database/mongoose/load-account-by-token.spec.ts
@@ -4,4 +4,8 @@ describe('MongooseLoadAccountByToken', () => {
   it('should be defined', () => {
     expect(new MongooseLoadAccountByToken()).toBeDefined()
   })
+
+  it('should have a method called load', () => {
+    expect(new MongooseLoadAccountByToken().load).toBeDefined()
+  })
 })
