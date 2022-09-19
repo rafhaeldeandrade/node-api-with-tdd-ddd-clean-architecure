09983438c9e8520bd6c68e8ece979022a6b21cc6
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 18:12:25 -0300

    feat: ensure MongooseLoadAccountByToken returns an account on success

--- src/infra/database/mongoose/load-account-by-token.ts
+++ src/infra/database/mongoose/load-account-by-token.ts
@@ -1,12 +1,24 @@
+import { LoadAccountByTokenRepository } from '@/data/contracts/database/load-account-by-token'
+import { AccountModel } from '@/domain/models/account'
 import { mongooseAccountModel } from '@/infra/database/mongoose/schemas/account'
 
-export class MongooseLoadAccountByToken {
-  async load(token: string): Promise<null> {
-    await mongooseAccountModel.findOne(
+export class MongooseLoadAccountByToken
+  implements LoadAccountByTokenRepository
+{
+  async load(token: string): Promise<AccountModel | null> {
+    const account = await mongooseAccountModel.findOne(
       { accessToken: token },
       {},
       { lean: true }
     )
-    return null
+    if (!account) return null
+    return {
+      id: account._id.toString(),
+      name: account.name,
+      email: account.email,
+      password: account.password,
+      role: account.role,
+      accessToken: account.accessToken
+    }
   }
 }
