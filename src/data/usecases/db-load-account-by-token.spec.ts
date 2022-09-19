64088e6671442075398289fb05350a843835c26f
Rafhael Gomes <rafhael@protonmail.com>
2022-09-19 16:00:05 -0300

    test: ensure DbLoadAccountByTokenUseCase calls loadAccountByTokenRepository with correct param

--- src/data/usecases/db-load-account-by-token.spec.ts
+++ src/data/usecases/db-load-account-by-token.spec.ts
@@ -1,16 +1,42 @@
+import { faker } from '@faker-js/faker'
 import { DbLoadAccountByTokenUseCase } from '@/data/usecases/db-load-account-by-token'
+import { LoadAccountByTokenRepository } from '@/data/contracts/database/load-account-by-token'
+import { AccountModel, Roles } from '@/domain/models/account'
+
+const fakeAccount = {
+  id: faker.datatype.uuid(),
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  role: faker.helpers.arrayElement([
+    'admin',
+    'moderator',
+    'writer',
+    'reader'
+  ]) as Roles
+}
+
+class LoadAccountByTokenRepositoryStub implements LoadAccountByTokenRepository {
+  async load(token: string): Promise<AccountModel | null> {
+    return fakeAccount
+  }
+}
 
 interface SutTypes {
   sut: DbLoadAccountByTokenUseCase
+  loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository
 }
 
 function makeSut(): SutTypes {
-  const sut = new DbLoadAccountByTokenUseCase()
+  const loadAccountByTokenRepositoryStub =
+    new LoadAccountByTokenRepositoryStub()
+  const sut = new DbLoadAccountByTokenUseCase(loadAccountByTokenRepositoryStub)
 
-  return { sut }
+  return { sut, loadAccountByTokenRepositoryStub }
 }
 
 describe('LoadAccountByToken Usecase', () => {
+  const fakeAccessToken = faker.datatype.uuid()
   it('should be defined', () => {
     const { sut } = makeSut()
 
@@ -21,5 +47,15 @@ describe('LoadAccountByToken Usecase', () => {
     const { sut } = makeSut()
 
     expect(sut.load).toBeDefined()
+  })
+
+  it('should call loadAccountByTokenRepository with correct param', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenRepositoryStub, 'load')
+
+    await sut.load(fakeAccessToken)
+
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(fakeAccessToken)
   })
 })
