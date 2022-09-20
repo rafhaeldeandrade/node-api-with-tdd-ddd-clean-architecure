3aab81212a9095f4df8f7c360b51189fac691f95
Rafhael Gomes <rafhael@protonmail.com>
2022-09-20 20:58:14 -0300

    refactor: add authorization middleware in POST /posts route

--- src/main/routes/post.routes.ts
+++ src/main/routes/post.routes.ts
@@ -1,7 +1,16 @@
 import { Router, RequestHandler } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeAddPostController } from '@/main/factories/add-post/makeAddPostController'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthenticationMiddleware } from '../factories/middlewares/makeAuthenticationMiddleware'
+import { Roles } from '@/domain/models/account'
 
 export default function (router: Router): void {
-  router.post('/posts', adaptRoute(makeAddPostController()) as RequestHandler)
+  const authorizedRoles = ['admin', 'moderator', 'writer'] as Roles[]
+  router.post(
+    '/posts',
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    adaptMiddleware(makeAuthenticationMiddleware(authorizedRoles)),
+    adaptRoute(makeAddPostController()) as RequestHandler
+  )
 }
